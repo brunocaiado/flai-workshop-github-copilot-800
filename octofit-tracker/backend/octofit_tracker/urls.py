87207361d337419120998a09c2c91bf17f64b0c1
Bruno Caiado <bcbrunocaiado8@gmail.com>
2026-02-19 16:27:08 +0000

    Configure base URL for Codespaces in urls.py

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from rest_framework import routers
 from octofit_tracker import views
+
+# Configure base URL for Codespaces
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
 
 # Create a router and register our viewsets
 router = routers.DefaultRouter()
